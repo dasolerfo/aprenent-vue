62efc21798aab74b943ccdcb69ad202d44210110
Daniel Soler <dasolerfo@gmail.com>
2025-06-10 13:38:42 +0200

    Dynamic Components creats en Vue

--- src/main.js
+++ src/main.js
@@ -2,13 +2,22 @@ import { createApp } from 'vue'
 
 //import App from './App.vue'
 //import App2 from './App2.vue'
-import App3 from './App3.vue'
+//import App3 from './App3.vue'
+import App4 from './App4.vue'
 
 import SlotComponent from './components/SlotComponent.vue'
 import FoodItem from './components/FoodItem.vue'
+import CompOne from './components/Component1.vue'
+import CompTwo from './components/Component2.vue'
+
+
 import './style.css'
 
-const app = createApp(App3)
+const app = createApp(App4)
 app.component('FoodItem', FoodItem)
 app.component('slot-comp', SlotComponent)
+
+app.component('comp-one', CompOne)
+app.component('comp-two', CompTwo)
+
 app.mount('#app')
